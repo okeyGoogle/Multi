68b25b2bb19348dc22f1d74948d156641683c31b
okeyGoogle <goalkeeper_94@mail.ru>
2019-12-15 22:16:01 +0600

    Update mxmulti.java

--- mxmulti.java
+++ mxmulti.java
@@ -1,48 +1,58 @@
 
 public class mxmulti {
-    public static void main(String args[]) {
-
-        int[][] m1 = new int[3][3];
-        int[][] m2 = new int[3][3];
-        int[][] mm;
-
-        System.out.println("Matrix 1 - ");
-        for (int row = 0; row < m1.length; row++) {
-            for (int col = 0; col < m1[row].length; col++) {
-                m1[row][col] = (int) Math.round(Math.random() * 10);
-                m2[row][col] = (int) Math.round(Math.random() * 10);
-                System.out.print(m1[row][col] + "   ");
+    public static void main(String[] args) {
+     
+     // =============Создание масивов=======================   
+        int[][]mat1 = new int[3][3];
+        int[][]mat2 = new int[3][3];
+        int[][]matRes = new int[3][3];
+     // ====================================================
+     
+     
+ 
+   
+   
+// =============Инициализация массива и доступ к его элементам и отрисовка 1 и 2 матрицы=======================
+        System.out.println("Mat1:");
+        for (int i = 0; i < mat1.length; i++) {
+            mat1[i] = new int[3];
+            mat2[i] = new int[3];
+            matRes[i] = new int[3];
+            for (int j = 0; j < mat1.length; j++) {
+                mat1[i][j] = (int) (Math.random()*10);
+                mat2[i][j] = (int) (Math.random()*10);
+                        System.out.print(mat1[i][j]+"  ");
             }
             System.out.println();
         }
-
-        System.out.println("Matrix 2 - ");
-        for (int row = 0; row < m2.length; row++) {
-            for (int col = 0; col < m2[row].length; col++) {
-                System.out.print(m2[row][col] + "   ");
-            }
-            System.out.println();
-        }
-
-        System.out.println("MUL - ");
-        if (m1[0].length == m2.length) {
-            mm = new int[m1.length][m2[0].length];
-            for (int i = 0; i < m1.length; i++) {
-                for (int j = 0; j < m2[0].length; j++) {
-                    for (int k = 0; k < m2.length; k++) {
-                        mm[i][j] += m1[i][k] * m2[k][j];
-                    }
+        System.out.println("Mat3:");
+        for (int i = 0; i < mat2.length; i++) {
+             for (int j = 0; j < mat2.length; j++) {
+                System.out.print(mat2[i][j]+"  "); 
+                }
+                System.out.println();
+             }        
+ // ====================================================        
+        
+// ===========Перемножение матриц===================
+        for (int i = 0; i < mat1.length; i++) {
+             for (int j = 0; j < mat2[i].length; j++) {
+                 for (int l = 0; l < mat2.length; l++) {
+            matRes [i][j] += mat1[i][l]*mat2[l][j];
                 }
             }
-        } else {
-            mm = null;
         }
+// ====================================================
 
-        for (int i = 0; i < mm.length; i++) {
-            for (int j = 0; j < mm[0].length; j++) {
-                System.out.print(mm[i][j] + "   ");
-            }
-            System.out.println();
+// ===========Вывод результата==========
+        
+          System.out.println("MatRes:");
+        for (int i = 0; i < matRes.length; i++) {
+             for (int j = 0; j < matRes.length; j++) {
+                System.out.print(matRes[i][j]+"  "); 
+                }
+                System.out.println();
+             }        
+// ====================================================
         }
     }
-}
